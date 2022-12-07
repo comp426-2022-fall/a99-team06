ca0a55630cf82403a9ceeb05b246b2b97fdd88bf
Zachary Cairns <zcairns@email.unc.edu>
2022-12-07 12:26:21 -0500

    added default / endpoint

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
+app.get('/', (req, res) => {
+        res.status(200).send("200 OK");
+})
+
+app.get('/app/', (req, res) => {
+	res.status(200).send("200 OK");
+})
+
 app.listen(port, () => {
 	console.log("App is live on 8080");
 });

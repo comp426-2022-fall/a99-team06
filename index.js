b644d1621db79b5b81918d80b5f582ad6ebee723
Zachary Cairns <zcairns@email.unc.edu>
2022-12-07 16:19:53 -0500

    redid some endpoints to make sense; login is now register

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const db = new database('users.db');
 db.pragma('journal_model = WAL');
 
-const createUserTable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, 'email' varchar, 'username' varchar, 'password' varchar);"
+const createUserTable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, 'username' varchar type UNIQUE, 'password' varchar);"
 db.exec(createUserTable);
 
 app.get('/', (req, res) => {
@@ -27,18 +27,24 @@ app.get('/app/login/', (req, res) => {
 	res.status(200).send("200 OK");
 })
 
-app.get('/app/register/:email/:username/:password/', (req, res) => {
-	let insert = "INSERT INTO users (email, username, password) VALUES ('"+ req.params.email + "'" + ", '" + req.params.username + "', '" + req.params.password + "');"
+app.get('/app/register/:username/:password/', (req, res) => {
+	let insert = "INSERT INTO users (username, password) VALUES ('" + req.params.username + "', '" + req.params.password + "');"
 	db.exec(insert); 
-	const stmt = db.prepare('SELECT * FROM users');
-	let row = stmt.all();
-	console.log(row);	
-	res.status(200).send("Created user:" + req.params.email +  req.params.username + req.params.password);
+	res.status(200).send("Created user: " +  req.params.username + ": " + req.params.password);
 })
 
 app.get('/app/truncate/', (req, res) => {
-	db.exec("TRUNCATE TABLE users");
+	db.exec("DROP TABLE users");
+	const createUserTable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, 'username' varchar type UNIQUE, 'password' varchar);"
+	db.exec(createUserTable);
 	res.status(200).send("you deleted all rows in table.");
+})
+
+app.get('/app/viewDB/', (req, res) => {
+        const stmt = db.prepare('SELECT * FROM users');
+        let row = stmt.all();
+        console.log(row); 
+	res.status(200).send("Viewing Database");
 })
 
 
